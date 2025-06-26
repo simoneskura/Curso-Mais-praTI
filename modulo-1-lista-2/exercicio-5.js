9eb7d381c06e849b37355dda1b859bca6ab3131f
Simone Skura <72580960+simoneskura@users.noreply.github.com>
2025-06-26 20:50:47 -0300

    cria exercício 5.js função debounce

---
+++ modulo-1-lista-2/exercicio-5.js
@@ -0,0 +1,10 @@
+function debounce(fn, delay) {
+  let timeoutI  return function(...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}    
